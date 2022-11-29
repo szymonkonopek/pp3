a850fd1c29604b5fd24c203a08fd4b604cfa7488
Szymon Konopek <sk.gornik@gmail.com>
2022-11-29 20:03:15 +0100

    07-AggregationAndComposition During Class

---
+++ 07-AggregationAndComposition/During Class/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+    private Writer writer;
+    private int numOfPages;
+    private String title;
+    private String category;
+    private int currPage;
+
+    public Book(Writer writer, int numOfPages, String title, String category, int currPage) {
+        this.writer = writer;
+        this.numOfPages = numOfPages;
+        this.title = title;
+        this.category = category;
+        this.currPage = currPage;
+    }
+
+    public void read(){
+
+    }
+
+    public void nextPage(){
+        this.currPage ++;
+    }
+
+    public void prevPage(){
+        this.currPage --;
+    }
+    
+    public void goToPage(int page){
+        this.currPage = page;
+    }
+}
---
+++ 07-AggregationAndComposition/During Class/Writer.java
@@ -0,0 +1,26 @@
+class Writer{
+    private String name;
+    private String surname;
+    private int age;
+    private boolean isFamous;
+
+    public Writer(String name, String surname, int age, boolean isFamous) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.isFamous = isFamous;
+    }
+
+
+    public void write(){
+
+    }
+
+    public void getFamous(){
+        this.isFamous = true;
+    }
+
+    public void goToStarbucksWithYourMackbook(){
+
+    }
+}
